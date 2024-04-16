40d0be488b86cb55a51a15b2a5b4938da3d96fef
Timothy Wever <timjwever@gmail.com>
2024-04-15 21:13:42 -0400

    refactored selected plans and combined some files

--- src/app/components/body-content/select-plan/SelectPlan.tsx
+++ src/app/components/body-content/select-plan/SelectPlan.tsx
@@ -1,37 +1,86 @@
-import Plan from '../../plans/Plan'
 import SubscriptionSwitch from '../../subscription-switch/SubscriptionSwitch'
 import styles from './selectPlan.module.scss'
 import IconArcade from '../../images/IconArcade'
 import IconAdvanced from '../../images/IconAdvanced'
 import IconPro from '../../images/IconPro'
+import { AVAILABLEPLANS } from '@/app/data/AvailablePlans'
+import { useMemo } from 'react'
+
+type PlanDetails = {
+    id: string
+    type: string
+    monthlyAmount: number
+    yearlyAmount: number
+}
 
 type SelectPlanProps = {
-    isYearly: boolean,
-    handleChange: () => void,
+    isYearly: boolean
+    selectedPlan: PlanDetails
+    setSelectedPlan: (plan: PlanDetails) => void
+    handleChange: () => void
 }
-const SelectPlan: React.FC<SelectPlanProps> = ({ isYearly, handleChange}) => {
+const SelectPlan: React.FC<SelectPlanProps> = ({
+    isYearly,
+    selectedPlan,
+    setSelectedPlan,
+    handleChange,
+}) => {
+    interface IconMapping {
+        [key: string]: JSX.Element
+    }
+
+    const iconMapping: IconMapping = useMemo(
+        () => ({
+            Arcade: <IconArcade />,
+            Advanced: <IconAdvanced />,
+            Pro: <IconPro />,
+        }),
+        []
+    )
+
     return (
         <div className={styles.planContent}>
-                <div className={styles.planContainer}>
-                    <Plan
-                        icon={<IconArcade />}
-                        plan={'Arcade'}
-                        amount={!isYearly ? '$9/mo': '$90/yr'}
-                        isYearly={isYearly}
-                    />
-                    <Plan
-                        icon={<IconAdvanced />}
-                        plan={'Advanced'}
-                        amount={!isYearly ? '$12/mo' : '$120/yr'}
-                        isYearly={isYearly}
-                    />
-                    <Plan icon={<IconPro />} plan={'Pro'} amount={'$15/mo'}
-                    isYearly={isYearly} />
-                </div>
-                <SubscriptionSwitch isYearly={isYearly} handleChange={handleChange} />
+            <div className={styles.planContainer}>
+                {AVAILABLEPLANS.map((plan) => (
+                    <div
+                        key={plan.index}
+                        className={styles.plan}
+                        onClick={() => {
+                            console.log(
+                                'you selected the ' + plan.type + ' plan'
+                            )
+                            console.log(plan)
+                            setSelectedPlan({
+                                id: plan.index,
+                                type: plan.type as 'Arcade' |
+                                    'Advanced' |
+                                    'Pro',
+                                monthlyAmount: plan.monthlyAmount,
+                                yearlyAmount: plan.yearlyAmount,
+                            })
+                        }}
+                    >
+                        <div>{iconMapping[plan.type]}</div>
+                        <div>
+                            <div className={styles.title}>{plan.type}</div>
+                            <div className={styles.amount}>
+                                {!isYearly
+                                    ? '$' + plan.monthlyAmount + '/mo'
+                                    : '$' + plan.yearlyAmount + '/yr'}
+                            </div>
+                            {isYearly && (
+                                <div className={styles.free}>2 months free</div>
+                            )}
+                        </div>
+                    </div>
+                ))}
             </div>
+            <SubscriptionSwitch
+                isYearly={isYearly}
+                handleChange={handleChange}
+            />
+        </div>
     )
 }
 
 export default SelectPlan
-
