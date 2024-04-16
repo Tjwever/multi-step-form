7b57fff5c056e736d43038cea05e58a27652cf92
Timothy Wever <timjwever@gmail.com>
2024-04-15 21:12:07 -0400

    added plan data

---
+++ src/app/data/AvailablePlans.ts
@@ -0,0 +1,20 @@
+export const AVAILABLEPLANS = [
+    {
+        index: '0',
+        type: 'Arcade',
+        monthlyAmount: 9,
+        yearlyAmount: 90,
+    },
+    {
+        index: '1',
+        type: 'Advanced',
+        monthlyAmount: 12,
+        yearlyAmount: 120,
+    },
+    {
+        index: '2',
+        type: 'Pro',
+        monthlyAmount: 15,
+        yearlyAmount: 150,
+    },
+]
