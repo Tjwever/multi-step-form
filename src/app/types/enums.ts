b69f584907811b033089458d6433add48a869b1e
Timothy Wever <timjwever@gmail.com>
2024-04-27 17:07:13 -0400

    refactor/added enums file

---
+++ src/app/types/enums.ts
@@ -0,0 +1,11 @@
+export enum NAVIGATION {
+    NONE = 'none',
+    NEXT = 'next',
+    BACK = 'goBack',
+    CONFIRM = 'confirm',
+}
+
+export enum DIRECTION {
+    BACK = 'back',
+    NEXT = 'next',
+}
