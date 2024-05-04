2fb546be96f2ebd95fe6e4a1106279e5e5aa50e5
Timothy Wever <timjwever@gmail.com>
2024-05-04 11:41:20 -0400

    added a loader spinner to mock uploaded/processing

--- src/app/components/body-content/thank-you/ThankYou.tsx
+++ src/app/components/body-content/thank-you/ThankYou.tsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './thankYou.module.scss'
 import IconThankYou from '../../images/IconThankYou'
+import PuffLoader from 'react-spinners/PuffLoader'
 
 const ThankYou: React.FC = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 2500)
+    }, [])
+
     return (
         <div className={styles.thankyouContainer}>
-            <IconThankYou />
-            <h1>Thank you!</h1>
-            <div>
-                Thanks for comfirming your subscription! We hope you have fun
-                using our platform. If you eer need support, please feel free to
-                email us at support@loremgaming.com.
-            </div>
+            {loading ? (
+                <div className={styles.loaderContainer}>
+                    <PuffLoader
+                        color={'rgb(71, 61, 255)'}
+                        loading={loading}
+                        size={150}
+                        aria-label='Loading Spinner'
+                        data-testid='loader'
+                    />
+                    <br />
+                    <div className={styles.text}>One moment please...</div>
+                </div>
+            ) : (
+                <>
+                    <IconThankYou />
+                    <h1>Thank you!</h1>
+                    <div className={styles.text}>
+                        Thanks for comfirming your subscription! We hope you
+                        have fun using our platform. If you eer need support,
+                        please feel free to email us at support@loremgaming.com.
+                    </div>
+                </>
+            )}
         </div>
     )
 }
